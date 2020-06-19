06458d28617fe1d62198b45b54261e8bcddaf398
adinochang <adinochang@users.noreply.github.com>
2020-06-19 17:52:18 +0800

    Change hard coded URIs to named routes

--- routes/web.php
+++ routes/web.php
@@ -14,34 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Home
-Route::get('/', 'EntryController@blog');
+Route::get('/', 'EntryController@blog')->name('home');
 
 
 
 // Questions
-Route::get('/question/', 'QuestionController@index');
+Route::get('/question/', 'QuestionController@index')->name('question.index');
 
-Route::get('/question/create/', 'QuestionController@create');
-Route::post('/question/', 'QuestionController@store');
+Route::get('/question/create/', 'QuestionController@create')->name('question.create');
+Route::post('/question/', 'QuestionController@store')->name('question.store');
 
-Route::get('/question/{question}', 'QuestionController@show');
+Route::get('/question/{question}', 'QuestionController@show')->name('question.show');
 
-Route::get('/question/edit/{question}', 'QuestionController@edit');
-Route::put('/question/{question}', 'QuestionController@update');
+Route::get('/question/edit/{question}', 'QuestionController@edit')->name('question.edit');
+Route::put('/question/{question}', 'QuestionController@update')->name('question.update');
 
-Route::delete('/question/{question}', 'QuestionController@destroy');
+Route::delete('/question/{question}', 'QuestionController@destroy')->name('question.destroy');
 
 
 
 // Entries
-Route::get('/entry/', 'EntryController@index');
+Route::get('/entry/', 'EntryController@index')->name('entry.index');
 
-Route::get('/entry/create/', 'EntryController@create');
-Route::post('/entry/', 'EntryController@store');
+Route::get('/entry/create/', 'EntryController@create')->name('entry.create');
+Route::post('/entry/', 'EntryController@store')->name('entry.store');
 
-Route::get('/entry/{entry}', 'EntryController@show');
+Route::get('/entry/{entry}', 'EntryController@show')->name('entry.show');
 
-Route::get('/entry/edit/{entry}', 'EntryController@edit');
-Route::put('/entry/{entry}', 'EntryController@update');
+Route::get('/entry/edit/{entry}', 'EntryController@edit')->name('entry.edit');
+Route::put('/entry/{entry}', 'EntryController@update')->name('entry.update');
 
-Route::delete('/entry/{entry}', 'EntryController@destroy');
+Route::delete('/entry/{entry}', 'EntryController@destroy')->name('entry.destroy');
